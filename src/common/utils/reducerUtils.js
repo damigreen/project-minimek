eaf33905a6d15a04f9c38918c083345f241d9caf
Damilola Faseun <fashfired@gmail.com>
2020-06-02 10:02:35 +0100

    Add createConditionalSlicereducer utility

--- src/common/utils/reducerUtils.js
+++ src/common/utils/reducerUtils.js
@@ -13,3 +13,23 @@ export function reduceReducers(...reducers) {
             previous
         );
 }
+
+export function createConditionalSliceReducer(sliceName, fnMap) {
+    // Create a reducer that know how to handle one lsice of state, with these action types
+    const sliceReducer = createReducer({}, fnMap);
+
+    // Creating a new wrapping reducer
+    return (state, action) => {
+        // Check to see if this slice reducer knows how to handle this action
+        if(fnMap[action.type]) {
+            // If it does, pass the slice to the slice reducer, and update the slice
+            return {
+                ...state,
+                [sliceName] : sliceReducer(state[sliceName], action),
+            };
+        }
+
+        // Otherwise return the existing state unchanged
+        return state;
+    }
+}
