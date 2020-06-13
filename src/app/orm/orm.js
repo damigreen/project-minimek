19a24e80bb632519c4df55340d8bdf1e834686ed
Damilola Faseun <fashfired@gmail.com>
2020-06-13 11:47:54 +0100

    Add a Unit model class

--- src/app/orm/orm.js
+++ src/app/orm/orm.js
@@ -2,11 +2,12 @@ import { ORM } from 'redux-orm';
 import Pilot from '../../features/pilots/Pilot'
 import Mech from '../../features/mechs/Mech'
 import MechDesign from '../../features/mechs/MechDesign'
+import Unit from '../../features/unitInfo/Unit'
 
 const orm = new ORM({
   stateSelector: state => state.orm,
 });
 
-orm.register(Pilot, Mech, MechDesign);
+orm.register(Pilot, Mech, MechDesign, Unit);
 
 export default orm;
---
+++ src/features/unitInfo/Unit.js
@@ -0,0 +1,31 @@
+import { Model, fk, attr, many } from 'redux-orm';
+
+export default class Unit extends Model {
+  // static modelName = "Unit";
+  static get fields() {
+    return {
+      id : attr(),
+      name : attr(),
+      affiliation : attr(),
+      color : attr(),
+      pilots : many('Pilot'),
+      mechs : many('Mech'),
+    }
+  }
+
+  static parse(unitData) {
+    const {Pilot, Mech} = this.session;
+    console.log(Pilot)
+    console.log(Mech)
+
+    const parseData = {
+      ...unitData,
+      pilots : unitData.pilots.map(pilotEntry => Pilot.parse(pilotEntry)),
+      mechs : unitData.mechs.map(mechEntry => Mech.parse(mechEntry))
+    }
+
+    return this.upsert(parseData);
+  }
+}
+
+Unit.modelName = 'Unit';
