d0b153d170d4e4398ac89e8f74977c0171a758a9
Damilola Faseun <fashfired@gmail.com>
2020-06-13 10:37:04 +0100

    Add UnitOrganization component into UnitInfo panel

--- src/features/unitInfo/UnitInfo/UnitInfo.jsx
+++ src/features/unitInfo/UnitInfo/UnitInfo.jsx
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import {
-    Segment
+    Segment,
+    Grid,
+    Header,
 } from 'semantic-ui-react';
 
 import UnitInfoForm from './UnitInfoForm';
+import UnitOrganization from './UnitOrganization'
 
 class UnitInfo extends Component {
   render() {
 
     return (
       <Segment>
-        <UnitInfoForm />
+        <Grid>
+          <Grid.Column width={10}>
+            <Header as="h3">Units of Organization</Header>
+            <UnitOrganization />
+          </Grid.Column>
+        </Grid>
+        <Grid>
+          <Grid.Column width={6}>
+            <Header as="h3">Edit Units</Header>
+            <UnitInfoForm />
+          </Grid.Column>
+        </Grid>
+
       </Segment>
+
     )
   }
 
