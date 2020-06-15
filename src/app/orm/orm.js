bd4371de0acc8bb94d581c89934778af50401eae
Damilola Faseun <fashfired@gmail.com>
2020-06-15 08:04:00 +0100

    Add a faction model class

--- src/app/orm/orm.js
+++ src/app/orm/orm.js
@@ -1,13 +1,14 @@
 import { ORM } from 'redux-orm';
-import Pilot from '../../features/pilots/Pilot'
-import Mech from '../../features/mechs/Mech'
-import MechDesign from '../../features/mechs/MechDesign'
-import Unit from '../../features/unitInfo/Unit'
+import Pilot from '../../features/pilots/Pilot';
+import Mech from '../../features/mechs/Mech';
+import MechDesign from '../../features/mechs/MechDesign';
+import Unit from '../../features/unitInfo/Unit';
+import Faction from '../../features/unitInfo/Faction';
 
 const orm = new ORM({
   stateSelector: state => state.orm,
 });
 
-orm.register(Pilot, Mech, MechDesign, Unit);
+orm.register(Pilot, Mech, MechDesign, Unit, Faction);
 
 export default orm;
