f5e08e3f5b1744542122011349da215c094a4c46
Damilola Faseun <fashfired@gmail.com>
2020-06-17 11:38:17 +0100

    Show current unit detail values in the Unit TOE tree

--- src/features/unitInfo/UnitOrganizationTree/UnitOrganizationTree.jsx
+++ src/features/unitInfo/UnitOrganizationTree/UnitOrganizationTree.jsx
@@ -13,20 +13,39 @@ import Lance from './Lance';
 const mapState = (state) => {
     const session = getEntitiesSession(state);
     const {Unit} = session;
-
+    
+    let unit, faction, lances;
+    
     const unitModel = Unit.all().first();
 
-    let lances;
-
     if(unitModel) {
+        unit = unitModel.ref;
+        faction = unitModel.affiliation.ref;
         lances = unitModel.lances.toRefArray().map(lance => lance.id);
     }
 
-    return {lances};
+    return {unit, faction, lances};
 }
 
+const UNKNOWN_UNIT = { name : 'UNKNOWN' };
 
-const UnitOrganizationTree = ({ lances = [] }) => {
+
+const UnitOrganizationTree = ({ unit = UNKNOWN_UNIT, faction = {},  lances = [] }) => {
+    const { name, color } = unit;
+    const { name : factionName } = faction
+
+    const colorBlock = <div 
+        style={{
+            marginLeft : 10,
+            backgroundColor : color,
+            border : '1px solid black',
+            height : 20,
+            width : 40
+        }}
+    />
+
+    const displyText = factionName ? `${name} / ${factionName}` : name; 
+
     const lanceEntries = lances.map(lanceID => (
         <Lance key={lanceID} lanceID={lanceID} />
     ))
@@ -36,7 +55,7 @@ const UnitOrganizationTree = ({ lances = [] }) => {
           <List.Item>
               <List.Icon name="cubes" />
               <List.Content>
-                  <List.Header>Black Widow Company</List.Header>
+                  <List.Header style={{display : 'flex'}}>{displyText} {colorBlock}</List.Header>
                   <List.List>
                     {lanceEntries}
                   </List.List>
